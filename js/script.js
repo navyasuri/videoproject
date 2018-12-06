1382a692f235c1d412e3592d6c7bb4e18c33f9ca
Nav <navyasuri@nyu.edu>
2018-12-06 12:32:52 +0400

    Working on insta requess

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import * as secret from "SECRETS";
+console.log("Script Started");
+
+var link = "https://api.instagram.com/v1/users/self/?access_token=364867749.4561375.4be49395decb48af99f2f713a0694e92";
+
+function getToken(username){
+    
+    $.param(params);
+}
